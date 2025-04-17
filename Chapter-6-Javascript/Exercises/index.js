a00392a8ad039a714f675dbd0eae84395c2c2d03
Sxottcobolt-MH <sxottcobolt@gmail.com>
2025-04-17 10:55:04 +0400

    Create index.js

---
+++ Chapter-6-Javascript/Exercises/index.js
@@ -0,0 +1,18 @@
+// Get references to input fields and elements
+const costInput = document.getElementById('costPerLiter');
+const litersInput = document.getElementById('liters');
+const calculateBtn = document.getElementById('calculateBtn');
+const totalCostDisplay = document.getElementById('totalCost');
+
+
+calculateBtn.addEventListener('click', () => {
+
+  const costPerLiter = parseFloat(costInput.value);
+  const liters = parseFloat(litersInput.value);
+
+
+  const totalCost = costPerLiter * liters;
+
+
+  totalCostDisplay.textContent = `Total: $${totalCost.toFixed(2)}`;
+});
